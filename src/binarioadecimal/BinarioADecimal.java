d63b7ee91207a814d19b50f2cf20ce3927a466a6
Sergio Cifuentes <sergiocifuentes1485@gmail.com>
2020-07-28 16:16:05 -0600

    Proyecto Terminado jflex & cup

--- src/binarioadecimal/BinarioADecimal.java
+++ src/binarioadecimal/BinarioADecimal.java
@@ -5,6 +5,11 @@
  */
 package binarioadecimal;
 
+import binarioadecimal.cup.AnalizadorSintactico;
+import java.io.StringReader;
+import java.util.Scanner;
+import binarioadecimal.jflex.AnalizadorLexico;
+
 /**
  *
  * @author sergio
@@ -15,7 +20,36 @@ public class BinarioADecimal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+        boolean salir = false;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.println("Ingrese un Numero Binario");
+            boolean opcionValida=true;
+            
+                String numero = scanner.nextLine();
+                AnalizadorLexico al = new AnalizadorLexico(new StringReader(numero));
+                        AnalizadorSintactico as = new AnalizadorSintactico(al);
+                try {
+                    as.parse();
+                } catch (Exception ex) {
+                    java.util.logging.Logger.getLogger(BinarioADecimal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+                }
+                do {
+                    opcionValida=true;
+                System.out.println("Deseas Ingresar Otro? Y/N");
+                String opcion = scanner.nextLine();
+                if (opcion.equalsIgnoreCase("y")) {
+                    salir=false;
+                } else if (opcion.equalsIgnoreCase("n")) {
+                    salir=true;
+                } else {
+                    System.out.println("Opcion Invalida");
+                    opcionValida=false;
+                }
+            } while (!opcionValida);
+
+        } while (!salir);
     }
     
 }
--- src/binarioadecimal/jflex/lexico.jflex
+++ src/binarioadecimal/jflex/lexico.jflex
@@ -1,6 +1,6 @@
 
-package octaladecimal.jflex;
-import octaladecimal.cup.Simbolos;
+package binarioadecimal.jflex;
+import binarioadecimal.cup.Simbolos;
 import java_cup.runtime.Symbol;
 %%
 %class AnalizadorLexico
@@ -11,13 +11,14 @@ import java_cup.runtime.Symbol;
 %column
 
 /*Identifiers*/
-Numero = [01234567]
+Bin = [01]
 
 
 %%//Reglas Lexicas
 
 <YYINITIAL>{
-        {Numero}                 {return new Symbol(Simbolos.OCTAL, yycolumn,yyline,yytext());}
+        "."                 {return new Symbol(Simbolos.PUNTO, yycolumn,yyline,yytext());}
+        {Bin}                 {return new Symbol(Simbolos.BIN, yycolumn,yyline,yytext());}
         [ \t\r\n\f]                            {}
         .                                            {return new Symbol(Simbolos.ERROR,yycolumn,yyline,yytext());} 
  
